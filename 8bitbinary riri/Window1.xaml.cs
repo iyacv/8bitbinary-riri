c1404c84080a6cd7422f34984275356749b0bc14
iyacv <gusteriann@gmail.com>
2024-03-16 22:51:12 +0800

    n

--- 8bitbinary riri/Window1.xaml.cs
+++ 8bitbinary riri/Window1.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Media;
 
 namespace _8bitbinary_riri
 {
@@ -19,6 +20,8 @@ namespace _8bitbinary_riri
     /// </summary>
     public partial class Window1 : Window
     {
+        private SoundPlayer MenuSound;
+
         public Window1()
         {
             InitializeComponent();
